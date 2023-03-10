a2f4bb5dc7dc431272f7239e5b0903e617cdd9e4
ampv16 <angelica.piedrahita8569@gmail.com>
2023-03-10 11:34:18 -0500

    fin seccion 26

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,21 +1,42 @@
+import { useEffect } from "react"
 import { Navigate, Route, Routes } from "react-router-dom"
 import { LoginPage } from "../auth"
 import { CalendarPage } from "../calendar"
+import { useAuthStore } from "../hooks"
 
 export const AppRouter = () => {
 
-  const authStatus = 'authenticated'
+  // console.log(getEnvVariables())
+  // se llama en este punto ya que se debe determinar si se quiere mostrar el login o el main dependiendo de si el token sigue siendo valido o no
+  const { status, ckeckAuthToken } = useAuthStore()
+
+  useEffect(() => {
+    ckeckAuthToken()
+  }, [])
+
+  if (status === 'cheking') {
+    return (
+      <h3>Cargando...</h3>
+    )
+  }
 
   return (
     <Routes>
-      
       {
-        (authStatus === 'not-authenticated')
-        ? <Route path="/auth/*" element={< LoginPage />} />
-        : <Route path="/*" element={< CalendarPage />} />
+        (status === 'not-authenticated')
+          ? (
+            <>
+              <Route path="/auth/*" element={< LoginPage />} />
+              <Route path="/*" element={<Navigate to='/auth/login' />} />
+            </>
+          )
+          : (
+            <>
+              <Route path="/" element={< CalendarPage />} />
+              <Route path="/*" element={<Navigate to='/' />} />
+            </>
+          )
       }
-
-      <Route path="/*" element={<Navigate to='/auth/login' />} />
     </Routes>
   )
 }
